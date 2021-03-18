ffdbe9ca5b4efd34c7ef0c243933034fa926fac0
e1on <trohimovkavad@gmail.com>
2021-03-18 20:49:10 +0200

    Add validation for CreateUser

--- src/Value/CreateUserValue.php
+++ src/Value/CreateUserValue.php
@@ -2,10 +2,21 @@
 
 namespace App\Value;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CreateUserValue
 {
     public function __construct(
+
+        #[Assert\NotNull]
+        #[Assert\NotBlank]
         public string $name,
+
+        #[Assert\NotNull]
+        #[Assert\NotBlank]
+        #[Assert\Email(
+            message: 'The email {{ value }} is not a valid email.',
+        )]
         public string $email,
     ) {}
 }
