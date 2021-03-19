4049116843849f2af33eec2df892e5a3e3905d5a
Yaroslav <yaroslav.petrov.dev@gmail.com>
2021-03-19 12:01:56 +0200

    Improve AbstractUserController, Setup parsing JSON request body

--- src/Controller/User/AbstractUserController.php
+++ src/Controller/User/AbstractUserController.php
@@ -5,17 +5,12 @@ namespace App\Controller\User;
 use App\Controller\AbstractRestController;
 use App\Entity\User;
 use App\Exception\User\UserNotFoundException;
-use App\Repository\UserRepository;
 
 abstract class AbstractUserController extends AbstractRestController
 {
-    public function __construct(
-        private UserRepository $repository
-    ) {}
-
     protected function findUser(string $id): User
     {
-        $user = $this->repository->find($id);
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
 
         if (!$user instanceof User) {
             throw UserNotFoundException::for($id);
